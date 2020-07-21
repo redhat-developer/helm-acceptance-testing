0b143875bc2d1015161a7ecd2ae3d650fff5f456
Akash Shinde <akashshinde159@gmail.com>
2020-07-22 01:21:41 +0530

    Add openshift cluster provider (#6)
    * Run acceptance tests against provided k8s cluster
    * Add openshift cluster provider

--- lib/Helm.py
+++ lib/Helm.py
@@ -2,10 +2,18 @@ import os
 import common
 from ClusterProvider import auth_wrap
 
-TEST_CHARTS_ROOT_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) +'/../testdata/charts')
+TEST_CHARTS_ROOT_DIR = os.path.abspath(os.path.dirname(
+    os.path.realpath(__file__)) + '/../testdata/charts')
+
 
 class Helm(common.CommandRunner):
+    def get_chart_path(self, test_chart):
+        provider = os.getenv("CLUSTER_PROVIDER", default='')
+        if provider == '':
+            return test_chart
+        return f'{TEST_CHARTS_ROOT_DIR}/{provider}-{test_chart}'
+
     def install_test_chart(self, release_name, test_chart, extra_args):
-        chart_path = TEST_CHARTS_ROOT_DIR+'/'+test_chart
-        cmd = 'helm install '+release_name+' '+chart_path+' '+extra_args
+        cmd = 'helm install '+release_name+' ' + \
+            self.get_chart_path(test_chart) + ' '+extra_args
         self.run_command(auth_wrap(cmd))
